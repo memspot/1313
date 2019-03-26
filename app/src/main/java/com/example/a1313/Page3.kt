1e1e1a30e2e077ed24ac9bc33f5c4725b7235fd8
memspot <emilytanaka13@gmail.com>
2019-03-26 19:13:23 -0300

    Rascunho atualizad - cores (checar), mais funcionalidades

--- app/src/main/java/com/example/a1313/Page3.kt
+++ app/src/main/java/com/example/a1313/Page3.kt
@@ -1,12 +1,24 @@
 package com.example.a1313
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Page3 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_page3)
+    }
+
+    fun ClickUmidade (view: View){
+        var intent:Intent=Intent (this, PageUmidade::class.java)
+        startActivity(intent)
+    }
+
+    fun ClickUV (view: View){
+        var intent:Intent=Intent(this, PageUV::class.java)
+        startActivity(intent)
     }
 }
