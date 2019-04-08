c3c875f490b3ea645d608edd2676b8d42a5651c8
ra00209742 <ra00209742@pucsp.edu.br>
2019-04-08 18:36:55 -0300

    jooj

--- app/src/main/java/com/example/a1313/Page3.kt
+++ app/src/main/java/com/example/a1313/Page3.kt
@@ -4,17 +4,23 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_page3.*
 
 class Page3 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_page3)
+
+
+        val i = intent
+        recebeNome.text=i.getStringExtra("nomeJogador")
     }
 
     fun ClickUmidade (view: View){
         var intent:Intent=Intent (this, PageUmidade::class.java)
         startActivity(intent)
+
     }
 
     fun ClickUV (view: View){
